518abb906f2a7d0d5269cd0589bfbb92e5d71196
Burak Mike <burakmahyk@gmail.com>
2022-08-08 17:22:38 +0300

    add todo form init

--- react/ToDo/src/App.tsx
+++ react/ToDo/src/App.tsx
@@ -1,32 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="mainContainer">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+        }}
+      >
+        <label htmlFor="todo">
+          <h1 className="center">Add new</h1>
+          <input type="text" id="newTodo" placeholder="Check pull requests" />
+        </label>
+        <button type="submit" className="outline">
+          Add
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </form>
     </div>
   )
 }
