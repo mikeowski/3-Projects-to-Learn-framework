1154b2e76f2ca45cc87aefcb6662f67a073d76c6
Burak Mike <burakmahyk@gmail.com>
2022-08-08 21:46:04 +0300

    reducer action type added

--- react/ToDo/src/lib/types.ts
+++ react/ToDo/src/lib/types.ts
@@ -2,3 +2,11 @@ export type todo = {
   title: string
   completed: boolean
 }
+
+export enum ActionType {
+  ADD_TODO = 'add_todo',
+  REMOVE_TODO = 'remove_todo',
+}
+export type Action =
+  | { type: ActionType.ADD_TODO; payload: todo }
+  | { type: ActionType.REMOVE_TODO; payload: todo }
