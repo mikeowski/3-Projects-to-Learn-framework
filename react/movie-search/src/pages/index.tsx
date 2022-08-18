55d9a201e0829424bc9703a73c78f061a8c00ccd
Burak Mike <burakmahyk@gmail.com>
2022-08-19 02:35:16 +0300

    initial search form design

--- react/movie-search/src/pages/index.tsx
+++ react/movie-search/src/pages/index.tsx
@@ -1,9 +1,74 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-
+import { useForm, SubmitHandler } from 'react-hook-form'
+import {
+  searchInputValidator,
+  SearchInputValidatorType,
+} from '../shared/searchInputValidator'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { trpc } from '../utils/trpc'
+import { useState } from 'react'
+import { Movie } from '../shared/types'
 const Home: NextPage = () => {
-  return <div></div>
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    reset,
+  } = useForm<SearchInputValidatorType>({
+    resolver: zodResolver(searchInputValidator),
+  })
+  const [searchResults, setSearchResults] = useState<Movie[]>([])
+  const year = new Date().getFullYear()
+  const years = Array.from(new Array(50), (val, index) => year - index)
+  const onSubmit: SubmitHandler<SearchInputValidatorType> = async (
+    submitData
+  ) => {
+    const { data } = trpc.useQuery(['search', submitData])
+    if (data) setSearchResults(data)
+    console.log(searchResults)
+    reset()
+  }
+
+  return (
+    <div className="mt-40 container flex text-lg flex-col justify-center items-center">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+        <label
+          htmlFor="text"
+          className="px-3 py-5 border rounded-lg border-gray-500"
+        >
+          <span className="text-xl">Search:</span>
+          <input
+            placeholder="Kill Bill"
+            {...register('query', { required: true })}
+            className="bg-inherit text-lg px-2 focus:outline-none border-b-2 focus:border-gray-600"
+          />
+        </label>
+        <div>
+          <label htmlFor="number" className="flex w-full justify-between">
+            <span>Year:</span>
+            <select {...register('year')}>
+              {years.map((year) => (
+                <option key={years.indexOf(year)} value={year}>
+                  {year}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="checkbox" className="flex w-full justify-between">
+            <span>Adult search</span>
+            <input {...register('adult')} type="checkbox" className="" />
+          </label>
+        </div>
+        {/* errors will return when field validation fails  */}
+
+        <input
+          type="submit"
+          className="hover:cursor-pointer px-4 py-2 border rounded-lg hover:border-gray-100 border-gray-500 transition-all"
+        />
+      </form>
+    </div>
+  )
 }
 
 export default Home
