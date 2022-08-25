7443d5367ea887232bd055882bd3838ae6fa55a1
Burak Mike <burakmahyk@gmail.com>
2022-08-25 22:11:57 +0300

    fix empty submit

--- react/movie-search/src/shared/searchInputValidator.ts
+++ react/movie-search/src/shared/searchInputValidator.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 
 export const searchInputValidator = z.object({
   adult: z.boolean(),
-  query: z.string(),
+  query: z.string().min(1),
 })
 
 export type SearchInputValidatorType = z.infer<typeof searchInputValidator>
