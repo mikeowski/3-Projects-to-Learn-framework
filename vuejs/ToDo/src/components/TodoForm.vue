4cc49d0be86c4bd71584f8a657c64653c2aa9c78
Burak Mike <burakmahyk@gmail.com>
2022-08-28 23:05:21 +0300

    enter to add todo

--- vuejs/ToDo/src/components/TodoForm.vue
+++ vuejs/ToDo/src/components/TodoForm.vue
@@ -5,6 +5,7 @@
       v-model="todoContent"
       type="text"
       class="border mx-8 focus:outline-none p-2 focus:border-gray-500 focus:ring-0 transition-all rounded-md shadow-md shadow-teal-300"
+      @keyup.enter="addTodo()"
     />
     <button
       class="font-bold hover:bg-teal-400 transition-all mx-44 text-center bg-teal-300 rounded py-2"
