8d5ff55c9f8af38b08b1a49a98a985d6090139fa
Burak Mike <burakmahyk@gmail.com>
2022-08-28 21:40:54 +0300

    fix state management with custom store

--- vuejs/ToDo/src/App.vue
+++ vuejs/ToDo/src/App.vue
@@ -2,20 +2,16 @@
 import { onMounted, ref, provide } from 'vue'
 import type { Todo } from './types'
 import TodoForm from './components/TodoForm.vue'
-const todos = ref<Todo[]>([])
-provide('todos', todos)
-onMounted(() => {
-  const localTodo = window.localStorage.getItem('todos')
-  if (localTodo) todos.value = JSON.parse(localTodo)
-})
+import CurrentTodos from './components/CurrentTodos.vue'
+import { store } from './store/todoStore'
+
+provide('store', store)
 </script>
 
 <template>
   <h1 class="text-center text-4xl font-bold">TODO</h1>
   <TodoForm />
-  <div v-for="todo in todos">
-    {{ todo.content }}
-  </div>
+  <CurrentTodos />
 </template>
 
 <style scoped></style>
