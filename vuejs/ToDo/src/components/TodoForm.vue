53dc3c135bed1b00a8efcf5122fc4d0db139675a
Burak Mike <burakmahyk@gmail.com>
2022-08-28 15:26:59 +0300

    refactor

--- vuejs/ToDo/src/components/TodoForm.vue
+++ vuejs/ToDo/src/components/TodoForm.vue
@@ -2,7 +2,7 @@
   <div class="mt-10 flex flex-col text-center w-full space-y-4">
     <span class="text-4xl letter">Add TODO</span>
     <input
-      v-model="todo.content"
+      v-model="todoContent"
       type="text"
       class="border mx-8 focus:outline-none p-2 focus:border-gray-500 focus:ring-0 transition-all rounded-md"
     />
@@ -16,16 +16,19 @@
 </template>
 
 <script setup lang="ts">
-import { inject, Ref } from 'vue'
+import { inject, ref, Ref } from 'vue'
 import { Todo } from '../types'
 const todos = inject<Ref<Todo[]>>('todos')
 
-const todo: Todo = {
-  content: '',
-  done: false,
-}
+const todoContent = ref('')
 const addTodo = () => {
-  if (todo.content.length != 0) todos!.value = [...todos!.value, todo]
+  if (todoContent.value.length != 0) {
+    todos!.value = [
+      ...todos!.value,
+      { content: todoContent.value, done: false },
+    ]
+  }
+  todoContent.value = ''
 }
 </script>
 
