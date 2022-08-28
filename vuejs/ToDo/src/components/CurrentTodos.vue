5b812ab54f59b83e56207c0f84b15d0556aae812
Burak Mike <burakmahyk@gmail.com>
2022-08-28 21:44:14 +0300

    checked todos split in two sections

--- vuejs/ToDo/src/components/CurrentTodos.vue
+++ vuejs/ToDo/src/components/CurrentTodos.vue
@@ -1,6 +1,19 @@
 <template>
+  <h2 class="text-center font-bold text-2xl mt-10">Live Todos</h2>
   <div
-    v-for="(todo, index) in store?.todoData.todos"
+    v-for="(todo, index) in store?.todoData.todos.filter((todo) => !todo.done)"
+    class="border-b py-2 px-4 mt-2 flex justify-between items-center break-words"
+    @click="store?.methods.updateTodo(todo)"
+  >
+    <p :class="'w-3/4 ' + (todo.done ? 'line-through' : '')">
+      {{ todo.content }}
+    </p>
+
+    <button @click="store?.methods.removeTodo(todo)">delete</button>
+  </div>
+  <h2 class="text-center font-bold text-2xl mt-10">Checked</h2>
+  <div
+    v-for="(todo, index) in store?.todoData.todos.filter((todo) => todo.done)"
     class="border-b py-2 px-4 mt-2 flex justify-between items-center break-words"
     @click="store?.methods.updateTodo(todo)"
   >
