be78bae171d9ba32919c3d19dcf72d33d049a2c3
Burak Mike <burakmahyk@gmail.com>
2022-08-28 15:00:16 +0300

    created todos state and provide it

--- vuejs/ToDo/src/App.vue
+++ vuejs/ToDo/src/App.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref, provide } from 'vue'
+import type { Todo } from './types'
+const todos = ref<Todo[]>([])
+provide('todos', todos)
+onMounted(() => {
+  const localTodo = window.localStorage.getItem('todos')
+  if (localTodo) todos.value = JSON.parse(localTodo)
+})
+</script>
 
-<template></template>
+<template>
+  <h1 class="text-center text-4xl font-bold">TODO</h1>
+</template>
 
 <style scoped></style>
