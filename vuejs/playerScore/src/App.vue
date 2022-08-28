ae5172a892e10f3f7f7b7defcf20bc956d030d0a
Burak Mike <burakmahyk@gmail.com>
2022-08-28 13:26:20 +0300

    created playerCard

--- vuejs/playerScore/src/App.vue
+++ vuejs/playerScore/src/App.vue
@@ -1,5 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PlayerCard from './components/PlayerCard.vue'
+</script>
 
-<template></template>
+<template>
+  <div
+    class="max-w-screen-lg h-screen mx-auto flex flex-col justify-evenly items-center"
+  >
+    <PlayerCard name="Burak" />
+    <PlayerCard name="Yaren" />
+  </div>
+</template>
 
 <style scoped></style>
---
+++ vuejs/playerScore/src/components/PlayerCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    class="sm:w-1/2 w-full mx-12 border text-4xl grid grid-cols-5 text-center py-4 px-2 rounded-md"
+  >
+    <button
+      @click="
+        () => {
+          score++
+        }
+      "
+      class="text-xl hover:underline hover:font-bold transition-all h-full w-full border-r"
+    >
+      UP
+    </button>
+    <div class="col-span-3">
+      <h1 class="w-full border-b">{{ name }}</h1>
+      <span class="text-red-600">{{ score }}</span>
+    </div>
+    <button
+      @click="
+        () => {
+          if (score > 0) score--
+        }
+      "
+      class="text-xl hover:underline hover:font-bold transition-all h-full w-full border-l"
+    >
+      DOWN
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const { name } = defineProps<{ name: string }>()
+const score = ref<number>(0)
+</script>
+
+<style scoped></style>
