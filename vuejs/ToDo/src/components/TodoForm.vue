dc3917050c54488349a7c79db95c273f0a538111
Burak Mike <burakmahyk@gmail.com>
2022-08-28 21:47:55 +0300

    now have fancy look 🎉

--- vuejs/ToDo/src/components/TodoForm.vue
+++ vuejs/ToDo/src/components/TodoForm.vue
@@ -4,7 +4,7 @@
     <input
       v-model="todoContent"
       type="text"
-      class="border mx-8 focus:outline-none p-2 focus:border-gray-500 focus:ring-0 transition-all rounded-md"
+      class="border mx-8 focus:outline-none p-2 focus:border-gray-500 focus:ring-0 transition-all rounded-md shadow-md shadow-teal-300"
     />
     <button
       class="font-bold hover:bg-teal-400 transition-all mx-44 text-center bg-teal-300 rounded py-2"
