9f6e46c42bbe6d2a6b64cd70e02e1c20ebb73cbd
Burak Mike <burakmahyk@gmail.com>
2022-08-31 02:53:24 +0300

    feature: created movie card

---
+++ vuejs/movie-search/src/components/MovieCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{ movie.title }}</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useMovieStore } from '../store/MovieStore'
+
+const { id } = defineProps<{ id: number }>()
+const store = useMovieStore()
+const movie = store.getMovieById(id)
+</script>
+
+<style scoped></style>
