4cc5765accb2fa6b9f5fc4ab2bfb83832737c49f
Burak Mike <burakmahyk@gmail.com>
2022-08-31 03:09:44 +0300

    fix: reactivity working

--- vuejs/movie-search/src/views/HomePage.vue
+++ vuejs/movie-search/src/views/HomePage.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="max-w-screen-lg mx-auto">
     <MovieSearchForm @search="search" />
+    <div class="grid grid-cols-3 gap-2 mt-10">
+      <MovieCard v-for="movie in movies" :movie="movie" />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import MovieSearchForm from '../components/MovieSearchForm.vue'
 import { useMovieStore } from '../store/MovieStore'
-import type { SearchInput } from '../types'
+import type { SearchInput, Movie } from '../types'
+import MovieCard from '../components/MovieCard.vue'
+import { ref } from 'vue'
 const store = useMovieStore()
-const movies = store.movies
-const search = (payload: SearchInput) => {
-  store.searchMovies(payload)
+const movies = ref<Movie[]>([])
+const search = async (payload: SearchInput) => {
+  await store.searchMovies(payload)
+  movies.value = store.movies
 }
 </script>
 
