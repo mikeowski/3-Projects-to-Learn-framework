7538afc98544155a3b59ad9fe34a6d9c7c1ba52b
Burak Mike <burakmahyk@gmail.com>
2022-08-31 03:56:27 +0300

    added header

---
+++ vuejs/movie-search/src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="w-full text-center py-4">
+    <a href="/" class="text-6xl">MovieSearch</a>
+  </header>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
