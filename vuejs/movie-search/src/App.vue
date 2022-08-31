b43e037422c70ad6ce79b1a18beaeb792f980c12
Burak Mike <burakmahyk@gmail.com>
2022-08-31 03:56:22 +0300

    refactor: more fancy look

--- vuejs/movie-search/src/App.vue
+++ vuejs/movie-search/src/App.vue
@@ -1,7 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from './components/Header.vue'
+</script>
 
 <template>
-  <RouterView />
+  <Header />
+  <RouterView class="max-w-screen-lg mx-auto" />
 </template>
 
 <style scoped></style>
--- vuejs/movie-search/src/views/MoviePage.vue
+++ vuejs/movie-search/src/views/MoviePage.vue
@@ -1,5 +1,33 @@
 <template>
-  <div>{{ movie?.title }} :{{ id }}</div>
+  <div v-if="!movie" class="text-center h-full w-full text-6xl">
+    Movie Not Found
+  </div>
+  <div v-else class="flex flex-col items-center justify-center text-center">
+    <div
+      class="flex flex-col sm:flex-row h-full items-center justify-center w-full"
+    >
+      <img
+        v-if="movie.poster_image"
+        :src="'https://image.tmdb.org/t/p/original' + movie.poster_image"
+        :alt="movie.title"
+        class="aspect-auto h-96 left-0"
+      />
+
+      <div
+        class="flex flex-col justify-center text-center w-full h-full px-4 font-bold"
+      >
+        <span v-if="movie.adult" class="text-2xl">+18</span>
+        <h2 class="text-white text-6xl">{{ movie.title }}</h2>
+        <span class="text-gray-300 text-lg">
+          {{ movie.release_date?.split('-').reverse().join('/') }}
+        </span>
+      </div>
+    </div>
+    <span class="text-4xl font-bold">Overview</span>
+    <div class="text-gray-200 mt-3 text-xl">
+      {{ movie.overview }}
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
